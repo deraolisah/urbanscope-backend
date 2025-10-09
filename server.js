aabd9e9b5c0e7f0c85cc37e23b2932082fcaeb32
deraolisah <deraolisah@gmail.com>
2025-10-09 11:52:25 +0100

    Vercel Configuration

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json("Niarobi Apartment Api");
+})
+
 app.use('/api/properties', propertyRoutes);
 
 
